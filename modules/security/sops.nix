5d28d0b37c90ed37f4607870ee5d621e7c66cc69
KreconyMakaron <55319736+KreconyMakaron@users.noreply.github.com>
2026-02-23 19:13:35 +0100

    fix: add systemd activation for sops, so that vpn keys activate

--- modules/security/sops.nix
+++ modules/security/sops.nix
@@ -4,6 +4,8 @@
     defaultSopsFile = ../../secrets.yaml;
     defaultSopsFormat = "yaml";
     age.keyFile = "${config.hm.xdg.configHome}/sops/age/keys.txt";
+    
+    useSystemdActivation = true;
 
     secrets = {
       "protonvpn/amsterdam" = { };
