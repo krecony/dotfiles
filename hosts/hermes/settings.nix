ec3a9444a3a2380092611fcd99611581950e99e7
krecony <55319736+krecony@users.noreply.github.com>
2026-02-27 10:07:57 +0100

    feat: add security to rpi

--- hosts/hermes/settings.nix
+++ hosts/hermes/settings.nix
@@ -22,11 +22,18 @@ with lib;
     generic-extlinux-compatible.enable = mkForce true;
   };
 
-  users.users.${config.core.user}.initialHashedPassword =
-    lib.mkForce "$y$j9T$LNwI1pBYkqt/2GlSfVGNd0$vSOxb4cWF6OMWl/lOSHshHWQQdaVjW5LxSAEQJfgZ.C";
+  users.users.${config.core.user} = {
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID289L4Tnf7hkSj81IbYqfzldWV9vs09gYe4lxP7w7eD github.driven504@passinbox.com"
+    ];
+    initialHashedPassword = lib.mkForce "$y$j9T$LNwI1pBYkqt/2GlSfVGNd0$vSOxb4cWF6OMWl/lOSHshHWQQdaVjW5LxSAEQJfgZ.C";
+  };
 
   style = {
     displayServer = "headless";
     theme = "everforest";
   };
+
+  security.nix-mineral.enable = true;
+  nix-mineral.extras.misc.usbguard.enable = mkForce false;
 }
