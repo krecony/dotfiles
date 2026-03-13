24baf7575c593f0d0b7f55a2f8973b1839bcb7a5
krecony <55319736+krecony@users.noreply.github.com>
2026-03-13 12:31:51 +0100

    feat: add house-scraper script

--- hosts/hermes/settings.nix
+++ hosts/hermes/settings.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 with lib;
@@ -27,6 +28,22 @@ with lib;
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID289L4Tnf7hkSj81IbYqfzldWV9vs09gYe4lxP7w7eD github.driven504@passinbox.com"
     ];
     initialHashedPassword = lib.mkForce "$y$j9T$LNwI1pBYkqt/2GlSfVGNd0$vSOxb4cWF6OMWl/lOSHshHWQQdaVjW5LxSAEQJfgZ.C";
+    linger = true;
+  };
+
+  systemd.user.services.house-scraper = {
+    description = "House scraper container";
+    wantedBy = [ "default.target" ];
+    after = [ "network-online.target" ];
+    wants = [ "network-online.target" ];
+    serviceConfig = {
+      Restart = "always";
+      RestartSec = "10s";
+      ExecStartPre = "-${pkgs.podman}/bin/podman rm -f house-scraper";
+      ExecStart = "${pkgs.podman}/bin/podman run --name house-scraper --replace --env-file /home/hermes/house/.env localhost/house-scraper:latest";
+      ExecStop = "${pkgs.podman}/bin/podman stop -t 30 house-scraper";
+      ExecStopPost = "-${pkgs.podman}/bin/podman rm -f house-scraper";
+    };
   };
 
   style = {
@@ -34,8 +51,10 @@ with lib;
     theme = "everforest";
   };
 
-  security.nix-mineral.enable = true;
-  nix-mineral.extras.misc.usbguard.enable = mkForce false;
+  security = {
+    sops.enable = false;
+    disableSUIDs = true;
+  };
 
   network.tailscale.enable = true;
 }
