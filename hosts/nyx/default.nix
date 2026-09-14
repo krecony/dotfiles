86e95f38adac2bd124408eae7bd25ba9f35b9d5a
krecony <55319736+krecony@users.noreply.github.com>
2026-09-14 19:27:44 +0200

    style: move hardware-config into default.nix

--- hosts/nyx/default.nix
+++ hosts/nyx/default.nix
@@ -1,10 +1,19 @@
-{ inputs, ... }:
+{ inputs, lib, modulesPath, config, ... }:
 {
   imports = [
     "${inputs.nixos-hardware}/lenovo/thinkpad/p14s/intel" # no gen6 configuration
-    ./hardware-configuration.nix
+		(modulesPath + "/installer/scan/not-detected.nix")
     ./disko.nix
     ./settings.nix
   ];
   system.stateVersion = "26.05";
+
+  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.intel.npu.enable = true;
+  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
