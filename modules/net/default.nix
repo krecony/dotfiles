4f59e420d82acb531580c4f1ffb28eb4bd7c8b87
krecony <55319736+krecony@users.noreply.github.com>
2026-09-19 16:45:21 +0200

    fix: remove unsafe SSL workaround

--- modules/net/default.nix
+++ modules/net/default.nix
@@ -1,5 +1,4 @@
 {
-  pkgs,
   mkImports,
   ...
 }:
@@ -8,16 +7,6 @@
     ./vpn.nix
     ./tailscale.nix
   ];
-  # reduce SECLEVEL to connect to wifi
-  systemd.services.wpa_supplicant.environment.OPENSSL_CONF = pkgs.writeText "openssl.cnf" ''
-    openssl_conf = openssl_init
-    [openssl_init]
-    ssl_conf = ssl_sect
-    [ssl_sect]
-    system_default = system_default_sect
-    [system_default_sect]
-    CipherString = DEFAULT@SECLEVEL=0
-  '';
 
   networking = {
     useDHCP = false;
